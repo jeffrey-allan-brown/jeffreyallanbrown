a06f2023dcd0e009d8612382ce7a9bf3787baede
Jeffrey Brown <jeffrey@jeffreyallanbrown.com>
2025-09-10 13:31:56 -0400

    basic structure and homepage

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "./globals.css";
+
+export const metadata = {
+  title: "jeffrey allan brown - full stack web developer",
+  description: "i'm jeffrey, a full-stack web developer based in ohio.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`antialiased mx-auto mt-8 lg:mx-auto aligns-center bg-neutral-100`}>
+        <Header />
+        <main className="container mx-auto flex-auto min-w-0 mt-6 flex flex-col px-2 sm:px-12 bg-neutral-100">
+            {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <section className="">
+      <p className="mb-4 text-7xl tracking-normal font-playfair">
+        <span className="font-bold">Hi, I'm Jeffrey</span> - I'm a perpetual hobbyist and professional tinkerer.
+      </p>
+      <p className="font-lato font-light text-3xl">Development | Reading | Music | Etc.</p>
+      <div className="my-8">
+        
+      </div>
+    </section>
+  );
+}
